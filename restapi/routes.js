c3395bd7ff52d09d2360ec04467623b8f50e0907
emushtaq <eshanmushtaq@gmail.com>
2016-04-14 13:39:48 +0200

    mongo setup and added gitignore

---
+++ restapi/routes.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+// Get the router
+var router = express.Router();
+
+// Middleware for all this routers requests
+router.use(function timeLog(req, res, next) {
+  console.log('Request Received: ', dateDisplayed(Date.now()));
+  next();
+});
+
+// Welcome message for a GET at http://localhost:8080/restapi
+router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the Mensa API' });   
+});
+
+module.exports = router;
+
+function dateDisplayed(timestamp) {
+    var date = new Date(timestamp);
+    return (date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+}
--- restapi/server.js
+++ restapi/server.js
@@ -1,4 +1,4 @@
-// Create an HTTP server by loading http module
+/* Create an HTTP server by loading http module
 var http = require('http');
 
 // Configure the HTTP server to respond identically to any request
@@ -11,4 +11,28 @@ var server = http.createServer(function (request, response) {
 server.listen(3000);
 
 // Put a friendly message on the terminal
-console.log("Mensa Server running at http://127.0.0.1:3000/");
+console.log("Mensa Server running at http://127.0.0.1:3000/");*/
+
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var routes     = require('./routes');
+var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/restdb');
+
+// express app will use body-parser to get data from POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set port
+var port = process.env.PORT || 8080;        // set the port
+
+// Define a prefix for all routes
+// Can define something unique like MyRestAPI
+// We'll just leave it so all routes are relative to '/'
+app.use('/', routes);
+
+// Start server listening on port 8080
+app.listen(port);
+console.log('MensaAPI listening on port: ' + port);
